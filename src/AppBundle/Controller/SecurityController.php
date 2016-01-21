f1ed4abdd2e8bba2cacf528b5cc5b2a79d47f2e6
alwintje <alwinkroesen@gmail.com>
2016-01-21 18:43:47 +0100

    Initial commit

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alwin Kroesen
+ * Date: 12-2-2015
+ * Time: 12:10
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Configuration;
+use UserBundle\Entity\User;
+use UserBundle\Entity\Role;
+
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login_route")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+    }
+
+    /**
+     * @Route("/register", name="registerPage")
+     */
+    public function registerAction(Request $request){
+        $form = $this->createRegisterForm();
+        $response = $this->handleTheRequest($form, $request);
+
+        return $this->render(
+            'security/register.html.twig',
+            array(
+                'form' => $form->createView(),
+                'response' => $response,
+            )
+        );
+    }
+
+    private function createRegisterForm(){
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('username', TextType::class, array('label' => 'Gebruikersnaam','attr'=> array('class'=>'form-control')))
+            ->add('email', EmailType::class, array('label' => 'E-mail','attr'=> array('class'=>'form-control')))
+            ->add('voornaam', TextType::class, array('label' => 'Voornaam','attr' => array('class'=>'form-control')))
+            ->add('tussenVoegsels', TextType::class, array('label' => 'Tussenvoegsels','attr'=> array('class'=>'form-control'),'required' => false))
+            ->add('achternaam', TextType::class, array('label' => 'Achternaam','attr'=> array('class'=>'form-control')))
+            ->add('adres', TextType::class, array('label' => 'Adres','attr'=> array('class'=>'form-control')))
+            ->add('woonplaats', TextType::class, array('label' => 'Woonplaats','attr'=> array('class'=>'form-control')))
+            ->add('telefoon', TextType::class, array('label' => 'Telefoon','attr'=> array('class'=>'form-control')))
+            ->add('password', RepeatedType::class, array('first_name'  => 'Wachtwoord', 'second_name' => 'Bevestig', 'type' => PasswordType::class, 'options'=>array('attr'=> array('class'=>'form-control'))))
+            ->add('save', SubmitType::class, array('label' => 'Register','attr'=> array('class'=>'btn btn-default')))
+            ->getForm();
+        return $form;
+    }
+    private function handleTheRequest($form,$request){
+
+        $form->handleRequest($request);
+        $response = "";
+        if ($form->isValid()) {
+            $user = new User();
+            $user->setUsername($form->get('username')->getViewData());
+            $user->setEmail($form->get('email')->getViewData());
+            $user->setVoornaam($form->get('voornaam')->getViewData());
+            $user->setTussenVoegsels($form->get('tussenVoegsels')->getViewData());
+            $user->setAchternaam($form->get('achternaam')->getViewData());
+            $user->setAdres($form->get('adres')->getViewData());
+            $user->setWoonplaats($form->get('woonplaats')->getViewData());
+            $user->setTelefoon($form->get('telefoon')->getViewData());
+
+            $pass = $form->get('password')->getViewData();
+
+            $encoder = $this->container->get('security.password_encoder');
+            $encoded = $encoder->encodePassword($user, $pass['Wachtwoord']);
+
+            $user->setPassword($encoded);
+            $user->setIsActive(true);
+
+            $em = $this->getDoctrine()->getManager();
+//            if($this->admin) {
+//                $role = $em->getRepository('UserBundle:Role')
+//                    ->findOneBy(array(
+//                        "role" => "ROLE_SUPER_ADMIN"
+//                    ));
+//            }else{
+//            }
+            $role = $em->getRepository('UserBundle:Role')
+                ->findOneBy(array(
+                    "role" => "ROLE_USER"
+                ));
+            $user->addRole($role);
+            $user->serialize();
+
+            $em->persist($user);
+            $em->flush();
+
+            $response = "Succesvol toegevoegd.";
+        }
+        return $response;
+    }
+
+
+}
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,433 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alwin Kroesen
+ * Date: 12-2-2015
+ * Time: 14:06
+ */
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * UserBundle\Entity\User
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="UserBundle\Entity\UserRepository")
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=100, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $voornaam;
+
+    /**
+     * @ORM\Column(type="string", length=8)
+     */
+    private $tussenVoegsels;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $achternaam;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $adres;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $woonplaats;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $telefoon;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Role", inversedBy="users")
+     *
+     */
+    private $roles;
+
+
+    public function __construct()
+    {
+        $this->isActive = true;
+        $this->roles = new ArrayCollection();
+        // may not be needed, see section on salt below
+        // $this->salt = md5(uniqid(null, true));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoles()
+    {
+        //return array('ROLE_USER');
+        return $this->roles->toArray();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasRole($role)
+    {
+        //return array('ROLE_USER');
+        foreach($this->roles->toArray() as $val){
+            if($val == $role){
+                return true;
+            }
+        }
+        return false;
+        //return $this->roles->toArray();
+    }
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->email,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->email,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+            ) = unserialize($serialized);
+    }
+
+    /**
+     * Add roles
+     *
+     * @param \UserBundle\Entity\Role $roles
+     * @return User
+     */
+    public function addRole(\UserBundle\Entity\Role $roles)
+    {
+        $this->roles[] = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Remove roles
+     *
+     * @param \UserBundle\Entity\Role $roles
+     */
+    public function removeRole(\UserBundle\Entity\Role $roles)
+    {
+        $this->roles->removeElement($roles);
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set voornaam
+     *
+     * @param string $voornaam
+     *
+     * @return User
+     */
+    public function setVoornaam($voornaam)
+    {
+        $this->voornaam = $voornaam;
+
+        return $this;
+    }
+
+    /**
+     * Get voornaam
+     *
+     * @return string
+     */
+    public function getVoornaam()
+    {
+        return $this->voornaam;
+    }
+
+    /**
+     * Set tussenVoegsels
+     *
+     * @param string $tussenVoegsels
+     *
+     * @return User
+     */
+    public function setTussenVoegsels($tussenVoegsels)
+    {
+        $this->tussenVoegsels = $tussenVoegsels;
+
+        return $this;
+    }
+
+    /**
+     * Get tussenVoegsels
+     *
+     * @return string
+     */
+    public function getTussenVoegsels()
+    {
+        return $this->tussenVoegsels;
+    }
+
+    /**
+     * Set achternaam
+     *
+     * @param string $achternaam
+     *
+     * @return User
+     */
+    public function setAchternaam($achternaam)
+    {
+        $this->achternaam = $achternaam;
+
+        return $this;
+    }
+
+    /**
+     * Get achternaam
+     *
+     * @return string
+     */
+    public function getAchternaam()
+    {
+        return $this->achternaam;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+
+    /**
+     * Set adres
+     *
+     * @param string $adres
+     *
+     * @return User
+     */
+    public function setAdres($adres)
+    {
+        $this->adres = $adres;
+
+        return $this;
+    }
+
+    /**
+     * Get adres
+     *
+     * @return string
+     */
+    public function getAdres()
+    {
+        return $this->adres;
+    }
+
+    /**
+     * Set woonplaats
+     *
+     * @param string $woonplaats
+     *
+     * @return User
+     */
+    public function setWoonplaats($woonplaats)
+    {
+        $this->woonplaats = $woonplaats;
+
+        return $this;
+    }
+
+    /**
+     * Get woonplaats
+     *
+     * @return string
+     */
+    public function getWoonplaats()
+    {
+        return $this->woonplaats;
+    }
+
+    /**
+     * Set telefoon
+     *
+     * @param string $telefoon
+     *
+     * @return User
+     */
+    public function setTelefoon($telefoon)
+    {
+        $this->telefoon = $telefoon;
+
+        return $this;
+    }
+
+    /**
+     * Get telefoon
+     *
+     * @return string
+     */
+    public function getTelefoon()
+    {
+        return $this->telefoon;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
