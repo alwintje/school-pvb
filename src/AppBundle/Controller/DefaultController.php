8d334cdc53b781251215d240bb4c9269f0f58aff
alwintje <alwinkroesen@gmail.com>
2016-01-22 22:28:00 +0100

    website

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Cursus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use UserBundle\Entity\User;
 
@@ -77,5 +82,61 @@ class DefaultController extends Controller
             'can_leave' => $canLeave,
 
         ]);
+    }
+    /**
+     * @Route("/account/edit", name="account")
+     */
+    public function accountAction(Request $request)
+    {
+        // replace this example code with whatever you need
+//        $em = $this->getDoctrine()->getManager();
+//        if($this->getUser() == null){
+//
+//
+//            $user = new User();
+//        }
+        $form = $this->createFormBuilder($this->getUser())
+            ->add('username', TextType::class, array('label' => 'Gebruikersnaam','attr'=> array('class'=>'form-control')))
+            ->add('email', EmailType::class, array('label' => 'E-mail','attr'=> array('class'=>'form-control')))
+            ->add('voornaam', TextType::class, array('label' => 'Voornaam','attr' => array('class'=>'form-control')))
+            ->add('tussenVoegsels', TextType::class, array('label' => 'Tussenvoegsels','attr'=> array('class'=>'form-control'),'required' => false))
+            ->add('achternaam', TextType::class, array('label' => 'Achternaam','attr'=> array('class'=>'form-control')))
+            ->add('adres', TextType::class, array('label' => 'Adres','attr'=> array('class'=>'form-control')))
+            ->add('woonplaats', TextType::class, array('label' => 'Woonplaats','attr'=> array('class'=>'form-control')))
+            ->add('telefoon', TextType::class, array('label' => 'Telefoon','attr'=> array('class'=>'form-control')))
+            ->add('save', SubmitType::class, array('label' => 'Wijzig','attr'=> array('class'=>'float')))
+            ->getForm();
+        $response = $this->handleTheRequest($form, $request);
+        return $this->render('security/register.html.twig', [
+                'form' => $form->createView(),
+                'response' => $response,
+                'edit' => true
+            ]);
+    }
+    private function handleTheRequest($form,$request){
+
+        $form->handleRequest($request);
+        $response = "";
+        if ($form->isValid()) {
+
+            $user = $this->getUser();
+            $user->setUsername($form->get('username')->getViewData());
+            $user->setEmail($form->get('email')->getViewData());
+            $user->setVoornaam($form->get('voornaam')->getViewData());
+            $user->setTussenVoegsels($form->get('tussenVoegsels')->getViewData());
+            $user->setAchternaam($form->get('achternaam')->getViewData());
+            $user->setAdres($form->get('adres')->getViewData());
+            $user->setWoonplaats($form->get('woonplaats')->getViewData());
+            $user->setTelefoon($form->get('telefoon')->getViewData());
+
+            $em = $this->getDoctrine()->getManager();
+            $user->serialize();
+
+            $em->persist($user);
+            $em->flush();
+
+            $response = "Succesvol toegevoegd.";
+        }
+        return $response;
     }
 }
